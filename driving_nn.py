8a59e03a1fb011c3d4352aa95f6ef5098d30f8a4
SherylHohman <SherylHohman@users.noreply.github.com>
2017-04-17 16:56:21 -0700

    read training_data into X_train and y_train sets

--- driving_nn.py
+++ driving_nn.py
@@ -6,6 +6,8 @@
 
 # Read data from CSV file
 import csv
+import cv2
+import numpy as np
 
 # TODO: add flags so can input custom driving_log_path, in case save several versions
 driving_log_filename = 'driving_log.csv'
@@ -20,4 +22,35 @@ with open(driving_log_path + driving_log_filename) as csvfile:
 print(lines[0], '\n')
 for field in lines[0]:
   print(field)
+
+# separate lines of data into types of data ?? dunno how to say this
+# gather features and "labels"
+# initialize feature set
+#images = []
+camera_1_images = []
+#camera_1_images, camera_2_images, camera_3_images = [[], [], []]
+
+#initialize measurements: outputs/label sets
+#measurements = []
+steering_angles = []    # values: (-1, 1)
+#throttle, brake, speed = [[], [], []]  # values: (0,1), (0), (0, 30)
+
+# line: [camera1_image_path, camera2_image_path, camera3_image_path, steering_angle, throttle, brake, speed]
+for line in lines:
+  # features (images)
+  image_path = str(line[0])
+  # load image using openCV
+  image = cv2.imread(image_path)
+  camera_1_images.append(image)
+
+  # "labels" (measurements)
+  steering_angle = float(line[3])
+  steering_angles.append(steering_angle)
+
+# convert to numpy arrays, and save as train and "label" datasets
+X_train = np.array(camera_1_images)
+y_train = np.array(steering_angles[:])
+
+print(X_train.shape)
+print(y_train.shape)
 
