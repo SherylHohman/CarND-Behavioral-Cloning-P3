ea9f093b3f1cdac521477fae3c5b305f8659effa
SherylHohman <SherylHohman@users.noreply.github.com>
2017-04-17 15:35:46 -0700

    create driving_nn.py - nn to clone driving behaviour

---
+++ driving_nn.py
@@ -0,0 +1,5 @@
+# neural network for behavioural cloning driving behaviour that was caputred in driving_log.csv
+# reads training data from driving_log.csv
+# trains a neural network from that data
+# saves trained model as model.h5
+# model.h5 can then be used to autonomously drive the car in the simulator that was used to produce the training data.
